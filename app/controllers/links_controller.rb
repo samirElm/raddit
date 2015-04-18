96d4ee90c6782aafb14d4410ce89ce43cc05a461
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2015-04-18 19:57:00 +0200

    Add style and delete useless files

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -16,44 +16,28 @@ class LinksController < ApplicationController
   def edit
   end
 
-  # POST /links
-  # POST /links.json
   def create
     @link = current_user.links.build(link_params)
 
-    respond_to do |format|
-      if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
-      else
-        format.html { render :new }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
+    if @link.save
+      redirect_to @link, notice: 'Link was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
   def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
+    if @link.update(link_params)
+      redirect_to @link, notice: 'Link was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /links/1
-  # DELETE /links/1.json
   def destroy
     @link.destroy
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to links_url, notice: 'Link was successfully destroyed.'
   end
 
   def upvote
@@ -69,13 +53,13 @@ class LinksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_link
-      @link = Link.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def link_params
-      params.require(:link).permit(:title, :url)
-    end
+  def set_link
+    @link = Link.find(params[:id])
+  end
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
+
 end
